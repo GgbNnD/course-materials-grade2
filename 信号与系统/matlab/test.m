e8d727a4fa535f8a6d2b25218a42be6e3ea26ec8
hexiang.huang <2477133369@qq.com>
2025-02-27 08:42:19 +0800

    feat: matlab资源

--- 信号与系统/matlab/test.m
+++ 信号与系统/matlab/test.m
@@ -1,23 +1,11 @@
 clc;
 clear;
-a = 1:12;
-b = reshape(a,3,4);
-
-c = zeros(3,4);
-c(:) = a(:);
-
-a = [1 2 3 ; 4 5 6 ; 7 8 9];
-rank(a);
-sum(a);
-
-
-x = randn(10000,5);
-mean(x);
-
-f = [1 1];
-i = 1;
-while f(i)+f(i+1)<1000
-    f(i+2) = f(i)+f(i+1);
-    i = i+1;
-end
-f
+t=0:pi/100:20*pi;
+ x=sin(t);
+ y=cos(t);
+ z=t.*sin(t).*cos(t);
+ plot3(x,y,z);
+ title('Line in 3-D Space');
+ xlabel('X');ylabel('Y');
+ zlabel('Z');
+ grid on;
